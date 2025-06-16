bdddcb1fc110c6cf382eb2068f47410c3c85e917
CathalF <112016029+CathalF@users.noreply.github.com>
2025-06-16 15:27:05 +0100

    Initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import './index.css';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Movie Swipe App
+      </h1>
+    </div>
+  );
+}
